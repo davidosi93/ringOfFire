ec342339eb89ead481a4e4e9d892ecaac1cda68a
davidosi93 <d.osipov93@googlemail.com>
2023-05-02 22:06:45 +0200

    startscreen and button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartscreenComponent } from './startscreen/startscreen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StartscreenComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
