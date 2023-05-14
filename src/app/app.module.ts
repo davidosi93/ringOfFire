fd885997af4a2f7855d0c1a05661115cc77709c3
davidosi93 <d.osipov93@googlemail.com>
2023-05-14 19:00:59 +0200

    first problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { GameInfoComponent } from './game-info/game-info.component';
 import { MatCardModule } from '@angular/material/card';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 
 
@@ -38,7 +43,11 @@ import { MatCardModule } from '@angular/material/card';
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-7d7c7',
+    appId: '1:141822953659:web:5f97f2e5075bee7351c19e',
+    storageBucket: 'ring-of-fire-7d7c7.appspot.com',
+    apiKey: 'AIzaSyB8UVr1p8iRbxZEpwZJL1fmOTvFpV1Ow3M',
+    authDomain: 'ring-of-fire-7d7c7.firebaseapp.com',
+    messagingSenderId: '141822953659',
+  },};
